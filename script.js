bcd2dc7bf6ed55158ccb63b48ad57916e40bd486
CruzOrtegaGabriela <gabrielacruzortega@MacBookGaby.local>
2024-11-13 21:35:56 -0500

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        if (name && email && message) {
+            alert(`¡Gracias, ${name}! Tu mensaje ha sido enviado con éxito.\nTe contactaremos pronto.`);
+            form.reset();
+        } else {
+            alert("Por favor, completa todos los campos.");
+        }
+    });
+});
